fef26fdf8b7f187d227d5be3df5c46e9bbb64ca1
Sergei Miasnikov <Sergei_Miasnikov@epam.com>
2017-10-02 15:39:50 +0300

    Initilal code upload

---
+++ Util.py
@@ -0,0 +1,148 @@
+# ___________________________________________________________________________
+# |																			|
+# | Copyright (c) 2017 Sergei Miasnikov										|
+# |																			|
+# | Permission is hereby granted, free of charge, to any person obtaining	|
+# | a copy of this software and associated documentation files (the			|
+# | "Software"), to deal in the Software without restriction, including		|
+# | without limitation the rights to use, copy, modify, merge, publish,		|
+# | distribute, sublicense, and/or sell copies of the Software, and to		|
+# | permit persons to whom the Software is furnished to do so, subject to	|
+# | the following conditions:												|
+# | 																		|
+# | The above copyright notice and this permission notice shall be			|
+# | included in all copies or substantial portions of the Software.			|
+# | 																		|
+# | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,			|
+# | EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF		|
+# | MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND					|
+# | NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE	|
+# | LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION	|
+# | OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION	|
+# | WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.			|
+# |_________________________________________________________________________|
+#
+
+
+class Utilities:
+
+	# Test execution status strings
+	statusFaied = "FAIL"
+	statusError = "ERR"
+	statusSkipped = "SKIP"
+	
+	reservedFields = ["", "name", "status", "classname", "message", "suitname"]
+
+	# Sanitise string to fit XML specification
+	@staticmethod
+	def sanitise(string):
+		return string.strip().replace("\"",'&quot;').replace("'", "&apos;").replace("<", "&lt;").replace(">", "&gt;").replace("&", "&amp;")
+		
+	# Returns JUnit xml content as string
+	@staticmethod
+	def getTestSuites(str):
+		str1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+		str2 =  "<testsuites>\n"
+		str3 = str
+		str4 =  "</testsuites>"
+		return str1 + str2 + str3 + str4;
+		
+	# Returns testSuit string
+	@staticmethod
+	def getTestSuite(name, testsNo, errorsNo, failuresNo, skippedNo, time, timestamp, propertiesStr, testsStr):
+		str1 =  "\t<testsuite name=\"" + name + "\" errors=\"" + errorsNo + "\" skipped=\"" + skippedNo + "\" tests=\"" + testsNo + "\" failures=\"" + failuresNo + "\" time=\"" + time + "\" timestamp=\"" + timestamp + "\">\n"
+		str2 =  "\t</testsuite>\n"
+		return str1 + propertiesStr + testsStr + str2;
+		
+	# Return testCase string
+	@staticmethod
+	def getTestCase(testName, className, time, status, message, other):
+		str1 = "\t\t<testcase classname=\"" + className + "\" name=\"" + testName + "\" time=\"" + time + "\">\n"
+		str1 = str1 + other
+		if Utilities.isSkipped(status):
+			str1 = str1 + "\t\t\t<skipped />\n"
+		if Utilities.isFailed(status):	
+			str1 = str1 + "\t\t\t<failure message=\"" + message + "\">Test failed</failure>\n"
+		if Utilities.isError(status):
+			str1 = str1 + "\t\t\t<error message=\"" + message + "\">Test failed</error>\n"
+		str2 = "\t\t</testcase>\n"
+		return str1 + str2
+		
+	# Return testcase string from a dictionary object
+	@staticmethod	
+	def getTestCaseFromDictionary(dict, suiteProperties):
+		time = "0"
+		message = ""
+		other = ""
+		if "time" in dict:
+			time = dict["time"]
+		if "message" in dict:
+			message = dict["message"]
+		for i in dict:
+			if Utilities.isNotReserved(i) and i not in suiteProperties:
+				other = other + "\t\t\t<" + i + ">" + str(dict[i]) + "</" + i + ">\n"
+		res = Utilities.getTestCase(dict["name"], dict["classname"], time, dict["status"], message, other)
+		return res
+
+	# Return testSuite properties as a string
+	@staticmethod	
+	def getSuiteProperties(dict):
+		if len(dict) == 0:
+			return ""
+		str1 = "\t\t<properties>\n"
+		str2 = "\t\t</properties>\n"
+		str = ""
+		for i in dict:
+			str = str + "\t\t\t<" + i + ">" + dict[i] + "</" + i + ">\n"
+		return str1 + str + str2
+		
+	# Define common properties of testcases in test suite
+	# If a value of a column is the same for all testcases in a suite,
+	# the value can be put into suite properties
+	@staticmethod
+	def getSuitePropertiesFromDictionary(dict):
+		res = {}
+		isFirst = True
+		for test in dict:
+			for i in test:
+				if Utilities.isNotReserved(i):
+					if isFirst: #adding all properties
+						res[i] = test[i]
+						isFirst = False
+					else: #just checking
+						if i in res:
+							if not test[i] == res[i]: #property value is different within a suite
+								del res[i]
+		return res
+		
+	# Check column mask for mandatory values
+	@staticmethod
+	def checkColumnMask(columnsMask, separator, mandatoryValues):
+		columns = columnsMask.lower().split(separator)		
+		result = True
+		for value in mandatoryValues:
+			if not value.lower() in columns:
+				print "Mandatory column " + value.upper() + " is not defined! Please define it in columns list."
+				result = False
+		return result
+		
+	# Check whether status is FAILED
+	@staticmethod
+	def isFailed(status):
+		return Utilities.statusFaied.lower() in status.lower()
+		
+	# Check whether status is ERROR
+	@staticmethod
+	def isError(status):
+		return Utilities.statusError.lower() in status.lower()
+		
+	# Check whether status is SKIPPED
+	@staticmethod
+	def isSkipped(status):
+		return Utilities.statusSkipped.lower() in status.lower()
+		
+	# Check whether property name is reserved
+	@staticmethod
+	def isNotReserved(fieldName):
+		return fieldName not in Utilities.reservedFields
+
---
+++ toJUnit.py
@@ -0,0 +1,162 @@
+# ___________________________________________________________________________
+# |																			|
+# | Copyright (c) 2017 Sergei Miasnikov										|
+# |																			|
+# | Permission is hereby granted, free of charge, to any person obtaining	|
+# | a copy of this software and associated documentation files (the			|
+# | "Software"), to deal in the Software without restriction, including		|
+# | without limitation the rights to use, copy, modify, merge, publish,		|
+# | distribute, sublicense, and/or sell copies of the Software, and to		|
+# | permit persons to whom the Software is furnished to do so, subject to	|
+# | the following conditions:												|
+# | 																		|
+# | The above copyright notice and this permission notice shall be			|
+# | included in all copies or substantial portions of the Software.			|
+# | 																		|
+# | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,			|
+# | EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF		|
+# | MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND					|
+# | NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE	|
+# | LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION	|
+# | OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION	|
+# | WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.			|
+# |_________________________________________________________________________|
+#
+
+
+import sys
+import os
+import glob
+import argparse
+from Util import Utilities
+
+# Arguments description for application help
+parser=argparse.ArgumentParser(
+    description="""Script parses test results in raw text table format (e.g. CSV) to standard JUnit xml results file.""",
+    epilog="""Good luck!""")
+
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument("-i", "--input", help="Input data location, either file(s) with test results or 'stdin' to parse from pipe. Multiple files supported, e.g. '*.txt'", required=True)
+requiredNamed.add_argument("-c", "--columns", help="""List of column names. Defauld separator is comma (','), unless set with -s parameter. 
+													Mandatory columns are: Name, Status, and Classname. Optional: Message.
+													Other columns are also supported, see documentation for details.""", required=True)
+
+parser.add_argument("-o", "--output", help="Destination JUnit xml. Default = 'junit.xml'", default="junit.xml")
+parser.add_argument("-s", "--separator", help="Separator used in query. Default = ',' (comma, corresponds CSV)", default=",")
+parser.add_argument("-ih", "--ignoreheader", help="Skip the first line in input file. Use it when table contains header.", action="store_true")
+parser.add_argument("-v", "--verbose", help="Put extended log to console. Note: log may become rather heavy.", action="store_true")
+
+args=parser.parse_args()
+
+# Get argument values
+inputFileLocation = args.input
+isStdIn = (inputFileLocation.lower() == "stdin")
+columnsMask = args.columns
+outputFileLocation = args.output
+separator = args.separator
+ignoreHeader = args.ignoreheader
+isVerbose = args.verbose
+mandatoryColumns = ["name", "status", "classname"]
+
+# Print something to console
+def printToConsole( obj ):
+	if isVerbose:
+		print str(obj)
+
+		
+########## STEP 1. Check column mask for mandatory columns ##########
+printToConsole("\nChecking the list of columns. Mandatory columns are:")
+for column in mandatoryColumns:
+	printToConsole("\t - " + column)
+if not Utilities.checkColumnMask(columnsMask, separator, mandatoryColumns):
+	quit()
+print "\nList of columns is valid"
+
+
+########## STEP 2. Parse files (or pipe input) into dictionary object ##########
+
+# Process a single line
+def processLines(f, dictionaries):
+	dict = {}
+	lineCount = 0
+	for line in f:
+		#Ignoring header (first line) if needed.
+		if lineCount == 0:
+			lineCount += 1
+			if ignoreHeader:
+				continue
+		columns = columnsMask.lower().split(separator)
+		values = line.split(separator)
+
+		for column in columns:
+			column = Utilities.sanitise(column.lower())
+				
+			#meaningless columns defined by 'null' or empty value
+			if column == "null" or column == "":
+				continue
+
+			if len(values) <= columns.index(column):
+				dict[column] = ""
+			else:
+				dict[column] = Utilities.sanitise(values[columns.index(column)])
+			
+		#'name' and 'classname' are mandatory. Ignoring lines without any of these fields.
+		if not (dict["name"] == "" or dict["classname"] == "" or dict["status"] == ""):
+			if dict["classname"] in dictionaries:
+				dictionaries[dict["classname"]].append(dict.copy())
+			else:
+				dictionaries[dict["classname"]] = []
+				dictionaries[dict["classname"]].append(dict.copy())
+
+dictionaries = {}
+
+if not isStdIn:
+	files = glob.glob(inputFileLocation)
+	print "\nInput files:" + str(files)
+	for file in files:
+		with open(file, 'r') as f:
+			processLines(f, dictionaries)
+else:
+	print "\nInput at StdIn"
+	processLines(sys.stdin, dictionaries)
+
+	
+########## STEP 3. Convert dictionary into result string ##########
+
+aGroupsStringsList = ""			
+
+for suite in dictionaries: # iterating test suites
+	tests = ""
+	testsNo = 0
+	errorsNo = 0
+	failuresNo = 0
+	skippedNo = 0
+	time = "0"
+	timestamp = "0"
+	suiteProperties = Utilities.getSuitePropertiesFromDictionary(dictionaries[suite])
+	suitePropertiesStr = Utilities.getSuiteProperties(suiteProperties)
+	
+	for i in dictionaries[suite]: # iterating testcase
+		testsNo += 1
+		if Utilities.isFailed(i["status"]):
+			failuresNo += 1
+		elif Utilities.isSkipped(i["status"]):
+			skippedNo += 1
+		elif Utilities.isError(i["status"]):
+			errorsNo += 1
+			
+		test = Utilities.getTestCaseFromDictionary(i, suiteProperties)
+		tests += test
+
+	suitString = Utilities.getTestSuite(suite, str(testsNo), str(errorsNo), str(failuresNo), str(skippedNo), time, timestamp, suitePropertiesStr, tests)
+	aGroupsStringsList = aGroupsStringsList + suitString
+	
+finalXML = Utilities.getTestSuites(aGroupsStringsList)
+
+print "\nParsing finished, result file: " + str(os.path.abspath(outputFileLocation))
+printToConsole("\nResult data:\n\n" + finalXML)
+	
+fo = open(outputFileLocation,'w')
+fo.write(finalXML) # python will convert \n to os.linesep
+fo.close()
+
